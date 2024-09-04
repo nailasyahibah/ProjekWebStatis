ddffbe02f8968f607f56e1ab1950291f2dcf0fdf
nailasyahibah <nailasahibah@gmail.com>
2024-09-04 09:48:42 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login</title>
+	<link rel="stylesheet" href="login.css">
+</head>
+<body>
+<form action="ceklogin.php" method="post">
+<div class="login">
+
+<h2>Form Login</h2>
+<div class="input-group">
+<input name="username" type="text" id="username" size="20">
+<span>Username</span>
+</div>
+<div class="input-group">
+<input name="password" type="text" id="password" size="20">
+<span>Password</span>
+</div>
+<div class="input-group">
+<input type="submit" name="text" id="submit" value="LOGIN"><a class="btn btn-primary" href="homepage.html"></a>
+</div>
+</div>
+</form>
+</body>
+</html>
+
+</body>
+</html>
